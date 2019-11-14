471700ed36aa6a1f4a7ba2727be52f9723ea0e20
Ian Pringle <ipringle2@liberty.edu>
2019-11-14 07:01:50 -0500

    Lintin'

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 // unneeded but it took me a few minutes so I don't wanna delete it...j
 // const OPERATORS: &'static [&'static str] = &["+", "-", "*", "/", "%"];
@@ -26,38 +26,46 @@ fn modulo(a: isize, b: isize) -> String {
 
 fn main() {
     let matches = App::new("calc.rs")
-                            .version("0.1")
-                            .author("Ian S. Pringle <ian@dapringles.com>")
-                            .about("Do math.")
-                            .arg(Arg::with_name("verbose")
-                                 .short("v")
-                                 .long("verbose")
-                                 .help("Sets verbose output"))
-                            .arg(Arg::with_name("a")
-                                 .help("Left number of equation")
-                                 .required(true)
-                                 .index(1))
-                            .arg(Arg::with_name("OPERATOR")
-                                 .help("Operator to evaluate numbers with")
-                                 .required(true)
-                                 .index(2))
-                            .arg(Arg::with_name("b")
-                                 .help("Right number of equation")
-                                 .required(true)
-                                 .index(3))
-                            .get_matches();
+        .version("0.1")
+        .author("Ian S. Pringle <ian@dapringles.com>")
+        .about("Do math.")
+        .arg(
+            Arg::with_name("verbose")
+                .short("v")
+                .long("verbose")
+                .help("Sets verbose output"),
+        )
+        .arg(
+            Arg::with_name("a")
+                .help("Left number of equation")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("OPERATOR")
+                .help("Operator to evaluate numbers with")
+                .required(true)
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("b")
+                .help("Right number of equation")
+                .required(true)
+                .index(3),
+        )
+        .get_matches();
 
-    let a: isize = matches.value_of("a")
-                                    .unwrap()
-                                    .parse()
-                                    .expect("Expected a number!");
-    let b: isize = matches.value_of("b")
-                                    .unwrap()
-                                    .parse()
-                                    .expect("Expected a number!");
-    let op: String = matches.value_of("OPERATOR")
-                                    .unwrap()
-                                    .to_string();
+    let a: isize = matches
+        .value_of("a")
+        .unwrap()
+        .parse()
+        .expect("Expected a number!");
+    let b: isize = matches
+        .value_of("b")
+        .unwrap()
+        .parse()
+        .expect("Expected a number!");
+    let op: String = matches.value_of("OPERATOR").unwrap().to_string();
 
     let result = match op.as_ref() {
         "+" => addition(a, b),
@@ -65,7 +73,7 @@ fn main() {
         "x" => multiplication(a, b),
         "/" => division(a, b),
         "%" => modulo(a, b),
-        _   => panic!("'{}' is an unsupported operator!", op),
+        _ => panic!("'{}' is an unsupported operator!", op),
     };
 
     match matches.occurrences_of("verbose") {
